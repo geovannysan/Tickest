436c0f102aca1460b7f50234fb71b936758d97d7
geovannysan <geovanny1santiago@gmail.com>
2022-09-03 12:23:25 -0500

    spiner

--- js/form.js
+++ js/form.js
@@ -19,17 +19,11 @@
 const container = document.querySelectorAll('p.evento');
 container.forEach(c => {
   c.addEventListener('click', (e) => {
-    //const dato = e.path[1].classList.contains("container")
-
-
     if (!e.target.localName == "p") {
       e.path[2].children[3].classList
-
       return
-
     };
     if (e.path[2].children[1].classList.contains("container")) {
-
       e.path[2].children[0].classList.toggle("d-none");
       e.path[2].children[1].classList.toggle("d-none")
       e.path[2].children[6].classList.toggle("d-none");
@@ -54,7 +48,7 @@ link.forEach(c => (c.addEventListener('click', (e) => {
     // e.path[2].children[7].classList.toggle("d-none");
 
   }
-  if (e.target.getAttribute('class').split(" ")[0] == "payphone") {
+  /*if (e.target.getAttribute('class').split(" ")[0] == "payphone") {
     // e.path[2].children[1].classList.toggle("d-none");
 
     // e.path[2].children[4].classList.toggle("d-none");
@@ -67,10 +61,10 @@ link.forEach(c => (c.addEventListener('click', (e) => {
   
      e.path[2].children[5].classList.toggle("d-none");
      e.path[2].children[6].classList.add("d-none");
-   e.path[2].children[7].classList.toggle("d-none");*/
+   e.path[2].children[7].classList.toggle("d-none");
     return
 
-  }
+  }*/
 })))
 const div = document.querySelectorAll('.back')
 const cancelar = document.querySelectorAll('.cancelar')
@@ -86,7 +80,7 @@ div.forEach(e => (e.addEventListener('click', (e) => {
   e.path[2].children[6].classList.add("d-none");
   e.path[2].children[7].classList.add("d-none");
 })))
-cancelar.forEach(e => (e.addEventListener('click', (e) => {
+/*cancelar.forEach(e => (e.addEventListener('click', (e) => {
   e.path[2].children[1].classList.toggle("d-none");
   e.path[2].children[0].classList.add("d-none");
   e.path[2].children[2].classList.add("d-none");
@@ -97,14 +91,13 @@ cancelar.forEach(e => (e.addEventListener('click', (e) => {
   e.path[2].children[7].classList.add("d-none");
 
 
-})))
+})))*/
 const validacedula = async (e) => {
   const numero = document.getElementById(e).value;
   $('#spinner').toggle('spinner')
 
   if (!numero) return
   try {
-    //const cedula = await  fetch("https://rec.netbot.ec/pdfqr/api/v1/cedula/"+numero+"",
     const { data } = await axios.get("https://rec.netbot.ec/pdfqr/api/v1/cedula/" + numero)
     const { message } = await data;
     const email = message['email'] ? message['email'] : '';
@@ -115,31 +108,15 @@ const validacedula = async (e) => {
     document.getElementById('date').value = edad
     document.getElementById('telefono').value = telefono
     document.getElementById('names').value = nombres
-
-
-    /*
-  {'headers': {'Access-Control-Allow-Origin': '*',}}).then(response => response.json());   
-    console.log(cedula)
-      const email = message['email']? message['email'] : '';
-      const edad = message['fecha_nacimiento'] ? cedula.message['fecha_nacimiento']: '';
-      const telefono =message['telefono'] ? cedula.message['telefono']: '';
-      const nombres = message['name']? message['name']:'';   
-     document.getElementById('email').value= email
-      document.getElementById('fecha').value=edad
-      document.getElementById('telefono').value=telefono
-      document.getElementById('nombre').value=nombres   
-      */
   } catch (Error) {
     //alert(Error,'Hubo un error en su Cédula')
-    console(Error)
     return Error;
 
   }
 }
 (function () {
   'use strict'
-
-  var forms = document.querySelectorAll('.needs-validation')
+  var forms = document.querySelectorAll('#report')
   const buttonElement = document.getElementById('td')
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
@@ -148,7 +125,6 @@ const validacedula = async (e) => {
           event.preventDefault()
           event.stopPropagation()
         }
-
         form.classList.add('was-validated')
       }, false)
     })
@@ -157,14 +133,10 @@ const validacedula = async (e) => {
 const reporte = document.getElementById('td')
 var form = document.querySelectorAll('.needs-validation')
 reporte.addEventListener('click', async function (event) {
-
   event.preventDefault()
-
   if (!document.getElementById('names').value || !document.getElementById('ce_dni').value || !document.getElementById('mail').value) return
   if (!document.getElementById('date') || !document.getElementById('banconom') || !document.getElementById('transacciones').value) return
   if (!document.getElementById('celular')) return
-  //variables de los datos
-
   let _datos = {
     u_nombre: document.getElementById('names').value,
     u_cedula: document.getElementById('ce_dni').value,
@@ -177,37 +149,16 @@ reporte.addEventListener('click', async function (event) {
   }
   try {
     const { data, status }  = await axios.post('https://rec.netbot.ec/pdfqr/api/v1/registro',_datos)
-   /* const registro = await fetch('http://localhost:4000/api/usuario', {
-      method: "POST",
-      body: JSON.stringify(_datos),
-      headers: { "Content-type": "application/json; charset=UTF-8" }
-    }).then(response => response.json())*/
-    const resultado = registro
     if (!data.success){
-      Swal.fire(
-          'Erro',
-          'hubo un error intente de nuevo',
-          'warning'
-        )}else{
-            Swal.fire(
-              'Su informacion a sido guardada',
-              'Espere a que un colaborador se contacte con usted',
-              'success'
-            )}
+      Swal.fire('Erro','hubo un error intente de nuevo','warning')
+        }else{
+            Swal.fire('Su informacion a sido guardada','Espere a que un colaborador se contacte con usted','success')
+          }
     $('#Modaltarjeta').modal('hide')
   } catch (error) {
-    Swal.fire(
-      'Erro',
-      'hubo un error intente de nuevo',
-      'warning'
-    )
+    Swal.fire('Erro','hubo un error intente de nuevo','warning')
     return error
-
-
   }
-
-
-
 })
 $('.cerrar').click(function () {
   $('#Modaltarjeta').modal('hide')
@@ -223,39 +174,28 @@ function reprtcompro(e) {
   document.getElementById('telefonos').value = ''
   document.getElementById('formularios').value = e;
   $('.titel').text(e);
-
 }
 const validacedula2 = async (e) => {
   const numero = document.getElementById(e).value;
-
   if (!numero) return
-
   try {
-
     const { data } = await axios.get("https://rec.netbot.ec/pdfqr/api/v1/cedula/" + numero)
     const { message } = await data;
     const email = message['email'] ? message['email'] : '';
     const edad = message['fecha_nacimiento'] ? message['fecha_nacimiento'] : '';
     const telefono = message['telefono'] ? message['telefono'] : '';
     const nombres = message['name'] ? message['name'] : '';
-
     document.getElementById('emaildos').value = email
     document.getElementById('fechados').value = edad
     document.getElementById('telefonodos').value = telefono
     document.getElementById('nombredos').value = nombres
-
   } catch (Error) {
-    //alert('Hubo un error en su Cédula')
-
-
     return Error;
-
   }
 }
 
 const eventos = async () => {
   const info = await fetch('http://localhost:4000/api/eventos').then(response => response.json())
-  console.log(info)
 }
 function handleSubmit(e) {
   e.preventDefault();
--- js/reportes.js
+++ js/reportes.js
@@ -4,37 +4,35 @@ const valida = async (e) => {
 
 
   if (!num) return
-
+  $('#spinner').find('span').removeClass('d-none')
   try {
-
     const { data } = await axios.get("https://rec.netbot.ec/pdfqr/api/v1/cedula/" + num)
     const { message } = await data;
     const email = message['email'] ? message['email'] : '';
     const edad = message['fecha_nacimiento'] ? message['fecha_nacimiento'] : '';
     const telefono = message['telefono'] ? message['telefono'] : '';
     const nombres = message['name'] ? message['name'] : '';
-
     document.getElementById('correos').value = email
     document.getElementById('fechas').value = edad
     document.getElementById('telefonos').value = telefono
     document.getElementById('nombres_apelli').value = nombres
-    
+    console.log(data.message.length)
+    //spinner.addClass('d-none')
+    $('#spinner').find('span').addClass('d-none')
+    if(!nombres) Swal.fire('','cedula no registrada','warning')
+    $('#spinner').find('span').addClass('d-none')
 
   } catch (Error) {
+    $('#spinner').find('span').addClass('d-none')
     Swal.fire(
       'Error',
       'Espere a que un colaborador se contacte con usted',
       'warning'
     )
-    console.log(Error)
-
     return Error;
-
   }
 }
-
 function reportatarjeta(e) {
-  //document.getElementById("report").reset();
   document.getElementById('nombres_apelli').value = ''
   document.getElementById('cedulas').value = ''
   document.getElementById('correos').value = ''
@@ -45,27 +43,15 @@ function reportatarjeta(e) {
   document.getElementById('telefonos').value = ''
 
   $('#Modaltarjeta').modal('show', { backdrop: 'static', keyboard: false });
-
+ 
+  
   document.getElementById('formulario').value = e;
   $('#titel').text(e);
 
 }
-
-
-
-
-const registro = async (info) => {
-
-
-
-
-
-}
-
 (function () {
   'use strict'
-
-  var forms = document.querySelectorAll('.needs-validation')
+  var forms = document.querySelectorAll('#tarjet')
   const buttonElement = document.getElementById('tc')
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
@@ -79,47 +65,40 @@ const registro = async (info) => {
       }, false)
     })
 })()
+
 const report = document.getElementById('tc')
 var form = document.querySelectorAll('.needs-validation')
 report.addEventListener('click', async function (event) {
-  const datos = [document.getElementById('nombres_apelli').value,
-  document.getElementById('cedulas').value,
-  document.getElementById('correos').value,
-  document.getElementById('fechas').value,
-  document.getElementById('telefonos').value]
   event.preventDefault()
-
   if (!document.getElementById('nombres_apelli').value || !document.getElementById('cedulas').value || !document.getElementById('correos').value) return
   if (!document.getElementById('fechas') || !document.getElementById('formulario') || !document.getElementById('telefonos').value) return
-  //variables de los datos
   let _datos = {
-    u_nombre: document.getElementById('nombres_apelli').value,
-    u_cedula: document.getElementById('cedulas').value,
-    u_correo: document.getElementById('correos').value,
-    u_fecha: document.getElementById('fechas').value,
-    u_banco: document.getElementById('bancos').value,
-    u_transaci: document.getElementById('comprobantes').value,
-    u_form: document.getElementById('formulario').value,
-    u_telefono: document.getElementById('telefonos').value
+    "u_nombre": document.getElementById('nombres_apelli').value,
+    "u_cedula": document.getElementById('cedulas').value,
+    "u_correo": document.getElementById('correos').value,
+    "u_fecha": document.getElementById('fechas').value,
+    "u_banco": document.getElementById('bancos').value,
+    "u_transaci": document.getElementById('comprobantes').value,
+    "u_form": document.getElementById('formulario').value,
+    "u_telefono": document.getElementById('telefonos').value
   }
   try {
-    const { data, status }  = await axios.post('https://rec.netbot.ec/pdfqr/api/v1/registro',_datos)
-    console.log(data,status)
-    if (data.success){
+    const { data, status } = await axios.post('https://rec.netbot.ec/pdfqr/api/v1/registro', _datos)
+    if (data.success) {
       Swal.fire(
         'Su informacion a sido guardada',
         'Espere a que un colaborador se contacte con usted',
         'success'
       )
       $('#Modaltarjeta').modal('hide')
-    }else{
-          Swal.fire(
-            'Erro',
-            'hubo un error intente de nuevo',
-            'warning'
+    } else {
+      Swal.fire(
+        'Erro',
+        'hubo un error intente de nuevo'+data.message,
+        'warning'
       )
     }
-    
+
   } catch (error) {
     Swal.fire(
       'Erro',
@@ -127,12 +106,7 @@ report.addEventListener('click', async function (event) {
       'warning'
     )
     return error
-
-
   }
-
-
-
 })
 $('.cerrar').click(function () {
   $('#Modaltarjeta').modal('hide')
